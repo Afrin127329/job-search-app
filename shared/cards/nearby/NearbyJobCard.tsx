fe0fb08a1b252ab0aed107f28ebce52cd34921df
Afrin Nahar <afrinnahar1999@gmail.com>
2023-09-20 22:14:36 +0600

    basic function written in nearbycard

---
+++ shared/cards/nearby/NearbyJobCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const NearbyJobCard = () => {
+  return (
+    <View>
+      <Text>NearbyJobCard</Text>
+    </View>
+  );
+};
+
+export default NearbyJobCard;
