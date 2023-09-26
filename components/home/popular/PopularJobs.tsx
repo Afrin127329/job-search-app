f95ab08868b1f2c0e2b4dbfe00aaaf17e26a0d1a
Afrin Nahar <afrinnahar1999@gmail.com>
2023-09-26 23:57:11 +0600

    updated index

--- components/home/popular/PopularJobs.tsx
+++ components/home/popular/PopularJobs.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
-export const PopularJobs = () => {
-  return (
-    <div>PopularJobs</div>
-  )
-}
+const PopularJobs = () => {
+  return <div>PopularJobs</div>;
+};
+
+export default PopularJobs;
--- components/index.tsx
+++ components/index.tsx
@@ -1,3 +1,6 @@
+// home screen
 import Nearbyjobs from "./home/nearby/Nearbyjobs";
+import PopularJobs from "./home/popular/PopularJobs";
+import Welcome from "./home/welcome/Welcome";
 
-export { Nearbyjobs };
+export { Nearbyjobs, PopularJobs, Welcome };
