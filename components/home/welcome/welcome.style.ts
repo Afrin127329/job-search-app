b7db6e91788765c92827a14ddbb3b08a7188daa7
Afrin Nahar <afrinnahar1999@gmail.com>
2023-09-30 19:43:22 +0600

    updated welcome styles

--- components/home/welcome/welcome.style.ts
+++ components/home/welcome/welcome.style.ts
@@ -2,6 +2,8 @@ import { StyleSheet } from "react-native";
 
 import { COLORS, FONT, SIZES } from "../../../constants";
 
+type tab = Function;
+
 const styles = StyleSheet.create({
   container: {
     width: "100%",
@@ -17,4 +19,56 @@ const styles = StyleSheet.create({
     color: COLORS.primary,
     marginTop: 2,
   },
+  searchContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    marginTop: SIZES.large,
+    height: 50,
+  },
+  searchWrapper: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+    marginRight: SIZES.small,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: SIZES.medium,
+    height: "100%",
+  },
+  searchInput: {
+    fontFamily: FONT.regular,
+    width: "100%",
+    height: "100%",
+    paddingHorizontal: SIZES.medium,
+  },
+  searchBtn: {
+    width: 50,
+    height: "100%",
+    backgroundColor: COLORS.tertiary,
+    borderRadius: SIZES.medium,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  searchBtnImage: {
+    width: "50%",
+    height: "50%",
+    tintColor: COLORS.white,
+  },
+  tabsContainer: {
+    width: "100%",
+    marginTop: SIZES.medium,
+  },
+  tab: (activeJobType: string, item: string) => ({
+    paddingVertical: SIZES.small / 2,
+    paddingHorizontal: SIZES.small,
+    borderRadius: SIZES.medium,
+    borderWidth: 1,
+    borderColor: activeJobType === item ? COLORS.secondary : COLORS.gray2,
+  }),
+  tabText: (activeJobType: string, item: string) => ({
+    fontFamily: FONT.medium,
+    color: activeJobType === item ? COLORS.secondary : COLORS.gray2,
+  }),
 });
+
+export default styles;
