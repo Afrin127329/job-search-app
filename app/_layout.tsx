31ce4cb93b1c48516342bcf40ea60054f5ab61f7
Afrin Nahar <afrinnahar1999@gmail.com>
2023-10-06 19:31:57 +0600

    Updated welcome and layout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,29 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+// import * as SplashScreen from "expo-splash-screen";
+
+// SplashScreen.preventAutoHideAsync();
+
+export const unstable_settings = {
+  initialRouteName: "home",
+};
 
 const Layout = () => {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Stack initialRouteName="home">
+      <Stack.Screen name="home" />
+    </Stack>
+  );
 };
 
 export default Layout;
--- components/home/welcome/Welcome.tsx
+++ components/home/welcome/Welcome.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Text, TextInput, View } from "react-native";
+import styles from "./welcome.style";
 
 const Welcome = () => {
-  return <div>Welcome</div>;
+  // const router = useRouter();
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Afrin</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job!</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} />
+        </View>
+      </View>
+    </View>
+  );
 };
 
 export default Welcome;
