8962a01ef6148302473b98d494855495ecf4a1d8
Afrin Nahar <afrinnahar1999@gmail.com>
2023-11-19 23:18:39 +0600

    Updated NearbyJobCard

--- shared/cards/nearby/NearbyJobCard.tsx
+++ shared/cards/nearby/NearbyJobCard.tsx
@@ -1,11 +1,31 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
-const NearbyJobCard = () => {
+import { checkImageURL } from "../../checkImageURL";
+import styles from "./nearbyjobcard.style";
+
+const NearbyJobCard = ({ job, handleNavigate }) => {
   return (
-    <View>
-      <Text>NearbyJobCard</Text>
-    </View>
+    <TouchableOpacity style={styles.container} onPress={handleNavigate}>
+      <TouchableOpacity style={styles.logoContainer}>
+        <Image
+          source={{
+            uri: checkImageURL(job.employer_logo)
+              ? job.employer_logo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          resizeMode="contain"
+          style={styles.logImage}
+        />
+      </TouchableOpacity>
+
+      <View style={styles.textContainer}>
+        <Text style={styles.jobName} numberOfLines={1}>
+          {job?.job_title}
+        </Text>
+
+        <Text style={styles.jobType}>{job?.job_employment_type}</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
