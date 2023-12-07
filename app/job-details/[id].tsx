f302c45154dfdc37e3ac6a9b2d9e302c4a56a08e
Afrin Nahar <afrinnahar1999@gmail.com>
2023-12-07 17:56:15 +0600

    created job-details route

---
+++ app/job-details/[id].tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+// import { Stack, useRouter, useSearchParams } from 'expo-router';
+// import { useCallback, useState } from 'react';
+
+// import { Company, JobAbout, JobFooter, JobTabs, Specifics } from '../../components';
+// import ScreenHeaderBtn from '../../shared/header/ScreenHeaderBtn';
+
+const JobDetails = () => {
+  return <Text>Job Details</Text>;
+};
+
+export default JobDetails;
--- components/index.tsx
+++ components/index.tsx
@@ -3,4 +3,24 @@ import NearbyJobs from "./home/nearby/NearbyJobs";
 import PopularJobs from "./home/popular/PopularJobs";
 import Welcome from "./home/welcome/Welcome";
 
-export { NearbyJobs, PopularJobs, Welcome };
+// Job Details screen
+import { default as JobAbout } from "./jobdetails/about/About";
+import Company from "./jobdetails/company/Company";
+import { default as JobFooter } from "./jobdetails/footer/Footer";
+import Specifics from "./jobdetails/specifics/Specifics";
+import { default as JobTabs } from "./jobdetails/tabs/Tabs";
+
+// Common export
+import NearbyJobCard from "../shared/cards/nearby/NearbyJobCard";
+
+export {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  NearbyJobCard,
+  NearbyJobs,
+  PopularJobs,
+  Specifics,
+  Welcome,
+};
